5116cf25d9283873679bdc5a4aa9ab6495006362
Samridhi_Monga <48271589+samgdb@users.noreply.github.com>
2019-09-16 22:15:48 +0530

    Create StringValidators.py
    Task: You are given a string s. Your task is to find out if the string contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
    Input Format: A single line containing a string s.
    Constraint: 0<length(s)<1000

---
+++ StringValidators.py
@@ -0,0 +1,6 @@
+str = input()
+print (any(s.isalnum() for s in str))
+print (any(s.isalpha() for s in str))
+print (any(s.isdigit() for s in str))
+print (any(s.islower() for s in str))
+print (any(s.isupper() for s in str))
